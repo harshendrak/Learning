d6e2e4de00d65a29d76e99dff831c9419745829b
harshendrak <harshendra.kumar2050@gmail.com>
2026-03-15 19:52:28 +0530

    learning embedding and chunking

---
+++ indexing.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+from google import genai
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings 
+from langchain_qdrant import QdrantVectorStore
+
+load_dotenv()
+
+
+pdf_path=Path(__file__).parent/"thinkpython2.pdf"
+
+# load this file in python program
+loader=PyPDFLoader(file_path=pdf_path)
+docs=loader.load()
+
+# Chunking the document into smaller pieces for better processing and embedding
+
+text_splitter=RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=400
+)
+chunks=text_splitter.split_documents(documents=docs)
+
+# vector embedding using Google's embedding model
+embedding_model=GoogleGenerativeAIEmbeddings(
+    model="gemini-embeddings-001")
+
+vector_db=QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    url="http://localhost:6333",
+    collection_name="RAG"
+)
+
+print("Indexing of documents done ....")
